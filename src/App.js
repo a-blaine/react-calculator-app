cdcd234fad3ae8c8592c0744624b9fe197c968a4
a-blaine <ai.blaine4@protonmail.com>
2023-06-25 14:56:34 +0100

    Updated digit buttons

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function reducer(state, { type, payload }) {
   }
 }
 
-let currentOperand = 4;
+let currentOperand = "";
 
 function App() {
   const [{ currentOperand, previousOperand, operation }, dispatch] = useReducer(
@@ -42,23 +42,23 @@ function App() {
         <button>DEL</button>
         <DigitButton digit="÷" dispatch={dispatch} />
 
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
+        <DigitButton digit="1" dispatch={dispatch} />
+        <DigitButton digit="2" dispatch={dispatch} />
+        <DigitButton digit="3" dispatch={dispatch} />
         <button>×</button>
 
-        <button>4</button>
-        <button>5</button>
-        <button>6</button>
+        <DigitButton digit="4" dispatch={dispatch} />
+        <DigitButton digit="5" dispatch={dispatch} />
+        <DigitButton digit="6" dispatch={dispatch} />
         <button>+</button>
 
-        <button>7</button>
-        <button>8</button>
-        <button>9</button>
+        <DigitButton digit="7" dispatch={dispatch} />
+        <DigitButton digit="8" dispatch={dispatch} />
+        <DigitButton digit="9" dispatch={dispatch} />
         <button>-</button>
 
         <button>.</button>
-        <button>0</button>
+        <DigitButton digit="0" dispatch={dispatch} />
         <button className="span-two">=</button>
       </div>
     </div>
